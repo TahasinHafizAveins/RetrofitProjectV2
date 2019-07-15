2cae73966fcd87ad07b956259be35136965a20c4
TahasinHafizAveins <tahasinaveins@gmail.com>
2019-07-15 17:30:16 +0600

    first commit

---
+++ app/src/main/java/com/example/retrofitprojectv2/JsonApi.java
@@ -0,0 +1,89 @@
+package com.example.retrofitprojectv2;
+
+import java.util.List;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.Field;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.HeaderMap;
+import retrofit2.http.Headers;
+import retrofit2.http.PATCH;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Url;
+
+public interface JsonApi {
+
+    // https://jsonplaceholder.typicode.com/posts
+   /* @GET("posts")
+    Call<List<Post>> getPosts();*/
+
+
+    //https://jsonplaceholder.typicode.com/posts?userId=4
+   /*@GET("posts")
+    Call<List<Post>> getPosts(@Query("userId") int userId);*/
+
+
+    //https://jsonplaceholder.typicode.com/posts?userId=4&_sort=id&_order=desc
+/*   @GET("posts")
+   Call<List<Post>> getPosts(
+           @Query("userId") Integer userId,
+           @Query("userId") Integer userId2,
+           @Query("_sort") String sort,
+           @Query("_order") String order
+           );//int not nullable Integer nullable*/
+
+    @GET("posts")
+    Call<List<Post>> getPosts(
+            @Query("userId") Integer[] userId,
+            @Query("_sort") String sort,
+            @Query("_order") String order
+    );//userID as a List
+
+    Call<List<Post>> getPosts(@QueryMap Map<String , String> parameters);
+
+
+    @GET("posts/{id}/comments")
+    Call<List<Comment>> getComments( @Path("id") int postid);
+
+    @GET
+    Call<List<Comment>>getComments(@Url String url);
+
+    @POST("posts")
+    Call<Post>createPost(@Body Post post);
+
+    @FormUrlEncoded
+    @POST("posts")
+    Call<Post>createPost(
+            @Field("userId") int userId,
+            @Field("title") String title,
+            @Field("body") String text
+    );
+
+    @FormUrlEncoded
+    @POST("posts")
+    Call<Post>createPost(@FieldMap Map <String, String> fields);
+
+    @Headers({"Static-Header1: 123","Static-Header2: 456"})
+    @PUT("posts/{id}")
+    Call<Post>putPost(@Header ("Dynamic-Header") String header,
+            @Path("id") int id ,
+            @Body Post post);
+
+    @PATCH("posts/{id}")
+    Call<Post>patchPost(@HeaderMap Map<String ,String>headers,
+                        @Path("id") int id ,
+                        @Body Post post);
+
+    @DELETE("posts/{id}")
+    Call<Void> deletePost(@Path("id") int id);
+}
